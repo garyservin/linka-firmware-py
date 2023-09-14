1bcf82f29c58890fe6128bb077a08257fe8fa364
Gary Servin <garyservin@gmail.com>
2023-09-14 00:02:13 -0400

    Minor fixes and add interval parameter to config

--- linka.py
+++ linka.py
@@ -1,7 +1,7 @@
 import sys
 import time
 
-if sys.platform == "esp8266" or sys.platform == "esp32":
+if sys.platform in ["esp8266", "esp32"]:
     import network
     import ntptime
     import ubinascii as binascii
@@ -11,17 +11,17 @@ elif sys.platform == "linux":
     import binascii
     import json
     import requests
+    import uuid
 
 VERSION = "linka_mpv0.0.1"
 
 
 class LinkaSensor:
+    DEFAULT_PUBLISH_INTERVAL = 300  # seconds
+    DEFAULT_SERVER_URL = "https://rald-dev.greenbeep.com/api/v1/measurements"
+
     def __init__(self, sensor, config):
         self.configure_network(config["ssid"], config["psk"])
-
-        if sys.platform == "esp8266" or sys.platform == "esp32":
-            # Configure time
-            ntptime.settime()
 
         self.sensor = sensor
         self.sensor_id = self.get_mac()
@@ -29,7 +29,8 @@ class LinkaSensor:
         self.description = config["description"]
         self.latitude = config["latitude"]
         self.longitude = config["longitude"]
-        self.linka_url = config["linka_url"]
+        self.linka_url = config.get("linka_url", self.DEFAULT_SERVER_URL)
+        self.interval = config.get("interval", self.DEFAULT_PUBLISH_INTERVAL)
         self.headers = {
             "Content-Type": "application/json; charset=utf-8",
             "x-api-key": self.api_key,
@@ -37,27 +38,31 @@ class LinkaSensor:
 
     @staticmethod
     def get_mac():
-        if sys.platform == "esp8266" or sys.platform == "esp32":
+        """Get MAC address of the client."""
+        if sys.platform in ["esp8266", "esp32"]:
             ap_if = network.WLAN(network.AP_IF)
             mac = binascii.hexlify(ap_if.config("mac")).decode()[-6:].upper()
         else:
-            import uuid
-
             mac = str(uuid.getnode())[-6:]
         return mac
 
     @staticmethod
     def configure_network(ssid, psk):
-        if sys.platform == "esp8266" or sys.platform == "esp32":
+        """Configure network if we're using a WiFi enabled board."""
+        if sys.platform in ["esp8266", "esp32"]:
             sta_if = network.WLAN(network.STA_IF)
             sta_if.active(True)
             sta_if.connect(ssid, psk)
             while not sta_if.isconnected():
                 time.sleep(1)
+
+            # Configure time
+            ntptime.settime()
         else:
             pass
 
     def read_data(self):
+        """Read data from sensor."""
         data = None
 
         pm_data = self.sensor.read()
@@ -84,15 +89,17 @@ class LinkaSensor:
         return data
 
     def publish(self, data):
+        """Publish data to backend server."""
         if data:
             print(f"Publishing to {self.linka_url} with: {data}")
             json_data = json.dumps(data)
-            r = requests.post(self.linka_url, data=json_data, headers=self.headers)
+            requests.post(self.linka_url, data=json_data, headers=self.headers)
         else:
             print("Unable to read data from sensor, not publishing")
 
     def run(self):
+        """Continues loop to read and publish data."""
         while True:
             data = self.read_data()
             self.publish(data)
-            time.sleep(300)
+            time.sleep(self.interval)
